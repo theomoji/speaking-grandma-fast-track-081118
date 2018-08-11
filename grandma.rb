c8a976c0c921f5a5f7528b3447d1b28976b04b37
Shuo Wang <gken90@gmail.com>
2018-08-11 22:59:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,9 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
